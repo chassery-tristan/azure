0d3150e853172b8dc16830282258e4494a05f804
chassery-tristan <76842162+chassery-tristan@users.noreply.github.com>
2024-05-17 18:29:36 +0200

    eede

--- tmp.sh
+++ tmp.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
-ssh -i keygen thomaseychenne47@34.155.238.191 << EOF
+ssh -i keygen thomaseychenne47@34.163.169.117 << EOF
+  if [ -z "$vmss_count" ]; then
+    vmss_count=1
+  fi
+  count="$vmss_count"
+  echo "Actual count: $count"
   sudo su stack
   cd ~/terraform
   terraform apply -auto-approve -lock=false
-  echo [PRIVATE] Launched ] > log*
-  ip=$(echo "$output" | grep -oP 'ip: \K.*')
-  db_port=3306
-  login=caca
-  password=gab
-  echo "Terraform apply completed successfully:"
-  echo "$output"
-  output=$(terraform apply -auto-approve tfplan Terraform/Web)
-  echo "Terraform apply completed successfully:"
-  echo "$output"
+  echo PRIVATE Launched > log
+  vmss_count=$((vmss_count + 1))
+  az vmss scale --resource-group myResourceGroup --name example-vmss --new-capacity 1
+  export vmss_count
 EOF
 
 echo "done"
